04bb85088a91ba2bdbfa98c6760d66d719581571
aleksh56 <averyanovv.a@yandex.ru>
2024-03-10 16:55:32 +0500

    Update start.js

--- client/scripts/start.js
+++ client/scripts/start.js
@@ -2,7 +2,7 @@ import path from 'path';
 import Hapi from '@hapi/hapi';
 import inert from '@hapi/inert';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 const FILES = /\.(js|js.map|woff|woff2|svg|bmp|jpg|jpeg|gif|png|ico|css)(\?v=\d+\.\d+\.\d+)?$/;
 
@@ -17,18 +17,22 @@ const init = async () => {
   server.route({
     method: 'GET',
     path: '/{path*}',
-    // handler: (request, h) => {
-    //   if (FILES.test(request.path)) {
-    //     return h.file(path.join(process.cwd(), 'dist', request.path));
-    //   }
-
-    //   return h.file(path.join(process.cwd(), 'dist', 'index.html'));
-    // },
     handler: (request, h) => {
-      const filePath = FILES.test(request.path)
-        ? path.join(__dirname, 'dist', request.path)
-        : path.join(__dirname, 'dist', 'index.html');
-      return h.file(filePath).code(FILES.test(request.path) ? 200 : 404);
+      console.log(`Request received for: ${request.path}`);
+
+      const matched = FILES.test(request.path);
+      console.log(`Does request match FILES regex? ${matched}`);
+
+      const filePath = matched
+        ? path.join(process.cwd(), 'dist', request.path)
+        : path.join(process.cwd(), 'dist', 'index.html');
+      console.log(`Returning file: ${filePath}`);
+
+      if (FILES.test(request.path)) {
+        return h.file(path.join(process.cwd(), 'dist', request.path));
+      }
+
+      return h.file(path.join(process.cwd(), 'dist', 'index.html'));
     },
   });
 
