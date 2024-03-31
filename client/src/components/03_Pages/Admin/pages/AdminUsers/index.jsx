591279e3710f9d3984eebf6eefb567b06b2b6769
aleksh56 <averyanovv.a@yandex.ru>
2024-03-31 17:18:09 +0500

    Fix profile data response

--- client/src/components/03_Pages/Admin/pages/AdminUsers/index.jsx
+++ client/src/components/03_Pages/Admin/pages/AdminUsers/index.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { getAllUsersFx } from './models/get-users';
 import CustomTable from '@/components/04_Widgets/table';
-import { getAdminUsersTableColumns } from '@/data/fields';
+// import { getAdminUsersTableColumns } from '@/data/fields';
+import BlockIcon from '@mui/icons-material/Block';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 const AdminUsersPage = () => {
   const [users, setUsers] = useState([]);
@@ -21,6 +23,49 @@ const AdminUsersPage = () => {
   const handleChangeStatus = (userId) => {
     console.log(`Сменить статус пользователя с ID: ${userId}`);
   };
+
+  const getAdminUsersTableColumns = (handleBlockUser, handleChangeStatus) => [
+    { id: 1, key: 'id', caption: 'ID' },
+    { id: 2, key: 'name', caption: 'Имя', render: (name) => name ?? '-' },
+    { id: 3, key: 'email', caption: 'Email' },
+    { id: 4, key: 'role', caption: 'Роль', render: (role) => role ?? '-' },
+    {
+      id: 5,
+      key: 'is_banned',
+      caption: 'Статус',
+      render: (isBanned) => (
+        <span
+          style={{
+            color: isBanned ? '#EF3826' : '#00B69B',
+            backgroundColor: isBanned ? 'rgba(239, 56, 38, .2)' : 'rgba(0, 182, 155, .3)',
+            padding: '6px 16px',
+            borderRadius: '5px',
+            fontWeight: 500,
+          }}
+        >
+          {isBanned ? 'Заблокирован' : 'Активен'}
+        </span>
+      ),
+    },
+    {
+      id: 6,
+      key: 'actions',
+      caption: 'Действия',
+      render: (_, user) => (
+        <>
+          <BlockIcon
+            onClick={() => handleBlockUser(user.id)}
+            color="error"
+            sx={{ cursor: 'pointer' }}
+          />
+          <SettingsIcon
+            onClick={() => handleChangeStatus(user.id)}
+            sx={{ ml: 1, cursor: 'pointer' }}
+          />
+        </>
+      ),
+    },
+  ];
 
   return (
     <CustomTable
