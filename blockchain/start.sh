5540e4e9c01687e407732aa2dbbd2fb7aa5a1e6e
aleksh56 <averyanovv.a@yandex.ru>
2024-04-25 17:10:41 +0500

    cp files after build

--- blockchain/start.sh
+++ blockchain/start.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-# Step 1: Stop ganache-cli if running
 pkill -f ganache-cli
 killall -q ganache-cli
 ps aux | grep ganache
 
-# Step 2: Run truffle migrate
-
-# Step 3: Start ganache-cli
 nohup ganache-cli --host 0.0.0.0 > ganache_output.txt 2>&1 &
 GANACHE_PID=$!
 
@@ -15,10 +11,8 @@ sleep 5
 
 truffle migrate
 
-# Wait a little for ganache to initialize
 sleep 5
 
-# Step 4 and 5: Monitor output for "Contract created:"
 CONTRACT_ADDRESS=""
 while IFS= read -r line
 do
@@ -30,12 +24,12 @@ do
   fi
 done < <(tail -f ganache_output.txt)
 
-# If the contract address was found, store it
 if [ -n "$CONTRACT_ADDRESS" ]; then
   echo $CONTRACT_ADDRESS > contract_address.txt
 else
   echo "Contract not found or ganache-cli did not output as expected."
 fi
 
-# Cleanup: Stop ganache-cli
-# kill $GANACHE_PID
+cp contract_address.txt /home/server/qpoll
+
+cp /home/blockchain/build/contracts/MiniPoll.json /home/server/qpoll
